9cac0a9bca9dcacc09d9f5b7df51f71ada869ad2
neeoll <arias.noel24@gmail.com>
2024-08-02 13:33:46 -0400

    Added external rollup option to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "../client",
+  root: '../client',
   build: {
-    outDir: "/dist"
+    outDir: "/dist",
+    rollupOptions: {
+      // Ensure server code is excluded
+      external: ['mongoose'],
+    }
   }
 })
