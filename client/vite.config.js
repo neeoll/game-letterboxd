1cebb61f8deee0522fe886931e11ebb6deb06278
neeoll <arias.noel24@gmail.com>
2024-08-02 14:08:13 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '../client',
+  root: '.',  // Indicates the root of the project is the client folder
   build: {
+    outDir: 'dist',  // Output directory for the build
     rollupOptions: {
-      // Ensure server code is excluded
+      // Ensure server-side dependencies are excluded
       external: ['mongoose'],
-    }
-  }
-})
+    },
+  },
+  resolve: {
+    alias: {
+      // Define any path aliases if needed
+      '@': '/src',
+    },
+  },
+});
