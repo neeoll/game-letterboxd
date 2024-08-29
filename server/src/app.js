2f3dc6574c51044309e722d2cbe6a8c4282cb9ad
neeoll <arias.noel24@gmail.com>
2024-08-29 14:40:30 -0400

    test cors fix

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,13 @@ import userRouter from "./routes/user.js"
 
 const corsConfig = {
   credentials: true,
-  origin: process.env.FRONTEND_URL
+  origin: true //process.env.FRONTEND_URL
 }
 
 const app = express()
 
-app.use(compression())
 app.use(cors(corsConfig))
+app.use(compression())
 app.use(cookieParser())
 app.use(express.json())
 app.use("/game", gamesRouter)
